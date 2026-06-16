ed43af7164fe743dca3e603dd3e2c7ac7fac5e92
xaxxoo <bnahmad83@gmail.com>
2026-06-16 08:41:45 +0100

    chore: wipe project for fresh start

--- .eslintrc.cjs
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  extends: ['plugin:@next/next/core-web-vitals', 'plugin:@typescript-eslint/recommended'],
-  ignorePatterns: ['.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
-};
